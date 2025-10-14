5ad66e22b50ce93fee1d29c23f5a82bcda5fb8db
hdgiacon <hectordorrighello@gmail.com>
2025-10-13 23:31:57 -0300

    tailwind e suporte a multiplos idiomas adicionado

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import HomePage from './pages/HomePage';
+import './i18n.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Suspense fallback={<div>Loading...</div>}>
+      <HomePage />
+    </Suspense>
   </StrictMode>,
 )
